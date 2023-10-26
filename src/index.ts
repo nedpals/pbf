b119203c1310f4fc5dffb726b451048a6a3ca26f
Ned Palacios <npdoesmc@gmail.com>
2023-10-26 10:40:50 +0800

    fix: export entire ops

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export type * from "./filter";
 export * from "./builder";
+export * from "./ops";
 export * from "./ops_utils";
 export { parseFilter as parse } from "./parser";
 export { stringifyFilter as stringify } from "./stringify";
